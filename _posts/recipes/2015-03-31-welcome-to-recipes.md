8c78530903e4cd51cfde01c303025e89f72bc579
JLBoor <jeanlouis.bourgeois@gmail.com>
2015-04-02 21:47:34 -0400

    implement head room

--- _posts/recipes/2015-03-31-welcome-to-recipes.md
+++ _posts/recipes/2015-03-31-welcome-to-recipes.md
@@ -3,4 +3,4 @@ layout: post
 title:  "Welcome to Recipes!"
 categories: recipes
 ---
-TODO
+
---
+++ js/init.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+    /**
+     * Initialize the menu so it's hides and shows the right elements.
+     * It also redirects the user to the right page when accessed from a post
+     */
+    var _initMenu = function() {
+
+        var hiddenClass = 'brick-hidden';
+        var categories = ['recipes', 'wellness', 'travel', 'life'];
+        var $logo = $('header .header-brand');
+
+        // Attach the filter to each navigation like
+        categories.forEach(function(category) {
+            $('.nav-' + category).on('click', function() {
+                $('.brick').addClass(hiddenClass);
+                $('.brick-' + category).removeClass(hiddenClass);
+            });
+        });
+
+        // The logo cancel the filters
+        $logo.on('click', function() {
+            $('.brick').removeClass(hiddenClass);
+        });
+    };
+
+
+    /**
+     * Initialize head room (scroll up reveal menu)
+     * using the right offset so that it's seamless
+     */
+    var _initHeadRoom = function() {
+        var myElement = document.querySelector("header");
+        var headroom  = new Headroom(myElement, {
+            offset : 213
+        });
+        headroom.init();
+    };
+
+
+    /**
+     * Execute the function
+     */
+    _initMenu();
+    _initHeadRoom();
+
+});
