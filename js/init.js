f282ec57a28bb96337e50af30ff66cbf6012d99e
JLBoor <jeanlouis.bourgeois@gmail.com>
2015-04-09 20:44:43 -0400

    add active class to navigation navigation between pages update friendly url to use none instead of date

--- js/init.js
+++ js/init.js
@@ -1,13 +1,46 @@
 $(document).ready(function() {
 
+    var  _getUrlCategory = function() {
+        return document.location.hash ? document.location.hash.substr(1) : null;
+    };
+
+    var _setUrlCategory = function(category) {
+        document.location.hash = '#' + (category || '');
+    };
+
+    var _goToHomePage = function(category) {
+        document.location = category ? '/#' + category : '/';
+    };
+
+
+    /**
+     * Highlight the active category by add the "active" class
+     * @param category the active category
+     */
+    var _activateMenu = function(category) {
+        $('.header-navigation .nav').removeClass('active');
+        if(category) {
+            $('.header-navigation .nav.nav-' + category).addClass('active');
+        }
+    };
+
     /**
      * Function that shows and hides bricks
      * @category if null, shows everything otherwise only the @category
      */
-    var _showBricks = function(category) {
+    var _showBricks = function(event, category) {
 
         var hideSelector;
         var showSelector = '.brick';
+        var $brickwall = document.querySelector('#brickwall');
+
+
+        if(!$brickwall && event) {
+            return _goToHomePage(category);
+        }
+
+        _activateMenu(category);
+        _setUrlCategory(category);
 
         if(category) {
             var categoryClass = showSelector + '-' + category;
@@ -27,28 +60,31 @@ $(document).ready(function() {
                 .addClass('brick-hidden');
         }
 
-        masonry = new Masonry(document.querySelector('#brickwall'), {
-            itemSelector: '.brick-visible',
-            columnWidth: '.brick-sizer'
-        });
+        // If there's a brick wall, when update the layout
+        if($brickwall) {
+            new Masonry($brickwall, {
+                itemSelector: '.brick-visible',
+                columnWidth: '.brick-sizer'
+            });
+        }
     };
 
     var _initMenu = function() {
 
         var hiddenClass = 'brick-hidden';
         var categories = ['recipes', 'wellness', 'travel', 'life'];
-        var $logo = $('header .header-brand');
+        var $logo = $('header .header-brand .nav-home');
 
         // Attach the filter to each navigation like
         categories.forEach(function(category) {
 
-            $('.nav-' + category).on('click', function() {
-                _showBricks(category)
+            $('.nav-' + category).on('click', function(event) {
+                _showBricks(event, category)
             });
         });
 
         // The logo cancel the filters
-        $logo.on('click', function() { _showBricks(); });
+        $logo.on('click', function(event) { _showBricks(event); });
     };
 
 
@@ -70,7 +106,7 @@ $(document).ready(function() {
      */
     _initMenu();
     _initHeadRoom();
-    _showBricks();
+    _showBricks(null, _getUrlCategory());
 
     $('.brick').on('mouseenter focusin', function() {
         $(this).siblings('.brick-visible').addClass('brick-ignored');
